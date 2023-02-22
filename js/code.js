04875d14393836e4d97e049ab1a3f6067165001a
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-22 09:27:42 +0200

    Add play function

--- js/code.js
+++ js/code.js
@@ -10,20 +10,51 @@ function getUserChoice() {
     let userInput = (prompt("Choose Rock, Paper, Scissors")).toLowerCase();
     if (!isValid(userInput)) {
         alert("Not valid, try again!");
-        getUserChoice();
+        return getUserChoice();
     } else {
-        return userInput
+        return userInput;
     };   
 };
 
 // Validate user choice
 function isValid(userChoice) {
-    const validOptions = ["rock", "paper", "scissors"]
+    const validOptions = ["rock", "paper", "scissors"];
     if (validOptions.includes(userChoice)) {
         return true;
     } else {
         return false;
     }
+};
+
+// Game
+function play() {
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+    console.log(`Player choice: ${userChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+    if (userChoice === computerChoice) {
+        alert("Draw! Play again");
+        return play();
+    } else if (userChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats scissors!");
+        } else if (computerChoice === "paper") {
+            console.log("Computer wins! Paper beats rock!");
+        };
+    } else if (userChoice === "scissors") {
+        if (computerChoice === "paper") {
+            console.log("You win! Scissors beat paper!");
+        } else if (computerChoice === "rock") {
+            console.log("Computer wins! Rock beats scissors!");
+        };
+    } else if (userChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats rock!");
+        } else if (computerChoice === "scissors") {
+            console.log("Computer wins! Scissors beat paper!");
+        };  
+    };
 }
 
-getUserChoice()
+play();
+
