f04ea364eebc553301d69048902d725cb7e32770
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-22 08:56:10 +0200

    Add user choice validation

--- js/code.js
+++ js/code.js
@@ -8,6 +8,22 @@ function getComputerChoice () {
 // Get user choice
 function getUserChoice() {
     let userInput = (prompt("Choose Rock, Paper, Scissors")).toLowerCase();
-    return userInput;
+    if (!isValid(userInput)) {
+        alert("Not valid, try again!");
+        getUserChoice();
+    } else {
+        return userInput
+    };   
+};
+
+// Validate user choice
+function isValid(userChoice) {
+    const validOptions = ["rock", "paper", "scissors"]
+    if (validOptions.includes(userChoice)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+getUserChoice()
