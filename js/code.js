92bcdf0834e8d2e92b30c43ca3e97410f296cd22
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-02-22 16:31:21 +0200

    Add Game func that play 5 rounds

--- js/code.js
+++ js/code.js
@@ -37,25 +37,43 @@ function playRound() {
     } else if (userChoice === "rock") {
         if (computerChoice === "scissors") {
             alert("You win! Rock beats scissors!");
+            userWins += 1;
         } else if (computerChoice === "paper") {
             alert("Computer wins! Paper beats rock!");
+            computerWins += 1;
         };
 
     } else if (userChoice === "scissors") {
         if (computerChoice === "paper") {
             alert("You win! Scissors beat paper!");
+            userWins += 1;
         } else if (computerChoice === "rock") {
             alert("Computer wins! Rock beats scissors!");
+            computerWins += 1;
         };
 
     } else if (userChoice === "paper") {
         if (computerChoice === "rock") {
             alert("You win! Paper beats rock!");
+            userWins += 1;
         } else if (computerChoice === "scissors") {
             alert("Computer wins! Scissors beat paper!");
+            computerWins += 1;
         };  
     };
 };
 
+// Play game of 5
+function game() {
+    for (let i=0; i<5; i++) {
+        playRound()
+    };
+};
 
+let userWins = 0;
+let computerWins = 0;
+
+game();
+
+alert(`User: ${userWins} - Computer: ${computerWins}`)
 
