4c726c62e9337241329de2cdf653b69d389fa51b
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-03-01 11:36:18 +0200

    Add updateResult function after each round

--- js/code.js
+++ js/code.js
@@ -8,7 +8,7 @@ function getComputerChoice () {
 // Round Play
 function playRound(userChoice) {
     let computerChoice = getComputerChoice();
-    const para = document.querySelector("#score-text")
+    const para = document.querySelector("#score-text");
     
     if (userChoice === computerChoice) {
         para.textContent = (`Draw! ${userChoice} = ${computerChoice}. Play again!`)
@@ -16,32 +16,38 @@ function playRound(userChoice) {
 
     } else if (userChoice === "rock") {
         if (computerChoice === "scissors") {
-            para.textContent = "You win! Rock beats scissors!";
+            para.textContent = "You win! Rock beats Scissors!";
             userWins += 1;
         } else if (computerChoice === "paper") {
-            para.textContent = "Computer wins! Paper beats rock!";
+            para.textContent = "Computer wins! Paper beats Rock!";
             computerWins += 1;
         };
 
     } else if (userChoice === "scissors") {
         if (computerChoice === "paper") {
-            para.textContent = "You win! Scissors beat paper!";
+            para.textContent = "You win! Scissors beat Paper!";
             userWins += 1;
         } else if (computerChoice === "rock") {
-            para.textContent = "Computer wins! Rock beats scissors!";
+            para.textContent = "Computer wins! Rock beats Scissors!";
             computerWins += 1;
         };
 
     } else if (userChoice === "paper") {
         if (computerChoice === "rock") {
-            para.textContent = "You win! Paper beats rock!";
+            para.textContent = "You win! Paper beats Rock!";
             userWins += 1;
         } else if (computerChoice === "scissors") {
-            para.textContent = "Computer wins! Scissors beat paper!";
+            para.textContent = "Computer wins! Scissors beat Paper!";
             computerWins += 1;
         };  
     };
+    updateResult(userWins, computerWins);
 };
+
+function updateResult(userWins, computerWins) {
+    const currentScore = document.querySelector("#current-score");
+    currentScore.textContent = `Player ${userWins} : Computer ${computerWins}`;
+}
 
 // Play game of 5
 function game() {
